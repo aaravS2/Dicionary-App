57ecd2927534597f6af9a04e4963fa5be8a836c4
aaravS2 <71128251+aaravS2@users.noreply.github.com>
2021-04-26 19:36:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,110 @@
+  
+import React, { Component } from 'react';
+import React from 'react';
+import { StyleSheet, Text, View,TextInput,TouchableOpacity } from 'react-native';
+import {Header} from 'react-native-elements'
+export default class App extends Component() {
+  constructor(){
+    super()
+    this.state={
+      text:"",
+          searchpressed:false,
+          word:"Loading...",
+          examples:[],
+          defination:"",
+          lexicalcatagory:"",
+    }
+  }
+  getvalue=(word)=>{
+var searchKeyword=word.toLowerCase()
+var url='https://rupinwhitehatjr.github.io/dictionary/%22'+searchKeyword+'%22.json'
+console.log(url)
+return fetch(url)
+.then((data)=>{
+  if (data.status===200) {
+    return data.json()
+  }
+  else{
+    return null
+  }
+})
+.then((response)=>{
+var responseOb=response
+if (responseOb) {
+  var wordData=responseOb.definations[0]
+  var Defination=wordData.description
+  var lexicalCat=wordData.wordtype
+  this.setState({
+    word:this.state.text,
+    defination:Defination,
+    lexicalcatagory:lexicalCat
+  })
+}else{
+  this.setState({
+    word:this.state.text,
+    defination:"Result(s) not found",
+    lexicalcatagory:"Definations not found hence category not found"
+  })
+}
+})
+  }
+  render(){
+  return (
+    <View>
+      <Header backgroundColor={'neon'}
+      centerComponent={{text:'Dictionary',style:{
+      color:'blue',
+      fontSize:20}}}/>
+      <TextInput
+      style={Styles.Inputbox}
+      onChangeText={text=>{
+        this.setState({
+          text:text,
+          searchpressed:false,
+          word:"Loading...",
+          examples:[],
+          defination:"",
+          lexicalcatagory:""
+        })
+      }}
+      value={this.state.text}
+      />
+        <TouchableOpacity style={Styles.Button} onPress={()=>{
+          this.setState({
+            searchpressed:true
+          })
+          this.getvalue(this.state.text)
+        }}>
+        <Text>Search</Text>
+        </TouchableOpacity>
+        <Text style={Styles.text}>{this.state.lexicalcatagory}</Text>
+        <Text style={Styles.text}>{this.state.text}</Text>
+        <Text style={Styles.text}>{this.state.defination}</Text>
+        <Text style={Styles.text}>{this.state.examples}</Text>
+    </View>
+  );
+  }
+}
+const Styles=StyleSheet.create({
+  Inputbox:{
+    marginTop:200,
+    width:'50%',
+    alignSelf:'center',
+    height:50,
+    textAlign:'center',
+    borderWidth:30,
+    borderRadius:40,
+  },
+  Button:{
+  borderRadius:39,
+  color:'yellow',
+  alignSelf:'center',
+  marginTop:100,
+  },
+  text:{
+    marginTop:100,
+    color:'neon',
+    textAlign:"center",
+    justifyContent:'center'
+  }
+})
